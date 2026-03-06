76296bede49c271f2878869386841e2d9061e426
Jose Almonte <jose.almonte@correo.unimet.edu.ve>
2026-03-05 20:28:06 -0400

    to github page

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";  // ✅ importamos Router
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* Añadimos el basename con el nombre exacto de tu repo */}
+    <BrowserRouter basename="/static-axie-release-frontend">
       <App />
     </BrowserRouter>
   </StrictMode>
